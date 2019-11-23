215373384e5b3d1676d6ebf9cd1a5161c349d227
cgomes1 <55416187+cgomes1@users.noreply.github.com>
2019-11-22 22:15:59 -0500

    css updates and password reset

--- mainJava.js
+++ mainJava.js
@@ -9,3 +9,30 @@ function formValidator(){
         return true;
     }
 }
+
+function checkForm(){
+    var oldPassword=document.forms["PasswordReset"]["oldpassword"].value;
+    var newPassword=document.forms["PasswordReset"]["newpassword"].value;
+    var confirmPassword=document.forms["PasswordReset"]["verifypassword"].value;
+
+    if(oldPassword!="" && newPassword!="" && confirmPassword!=""){
+        if(oldPassword!=newPassword){
+            if(newPassword==confirmPassword){
+                alert("Password has been updated");
+                return true;
+            }
+            else {
+                alert("New password needs to match");
+                return false;
+            }
+        }
+        else{
+            alert("New password cannot be same as old password");
+            return false;
+        }
+    }
+    else {
+        alert("All fields are required");
+        return false;
+    }
+}
