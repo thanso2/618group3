dd823f59d53fdc656dd649b240c594baee3d4e70
thanso2 <55329206+thanso2@users.noreply.github.com>
2019-11-24 17:29:19 -0500

    added express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
+
+
+app.listen(80, function(){
+  console.log("Listening on port 3000!")
+});
+
+
+app.get('/', function(req, res){
+    res.sendFile('index.html');
+  }); 
