f18ba2c9fadf8ada52c7bfa2be37659c0378e2e9
thanso2 <55329206+thanso2@users.noreply.github.com>
2019-11-25 06:02:02 -0500

    nodemailer

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const nodeMailer = require('nodemailer');
 
 app.use(express.static('public'));
 
@@ -9,4 +10,24 @@ app.listen(80, function(){
 
 app.get('/', function(req, res){
     res.sendFile('index.html');
-  }); 
+  });
+
+
+app.post('/messages.html', function(req, res, next) {
+    let transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'ait618group3@gmail.com',
+            pass: 'T0ws0n@iT618'
+        }
+    })
+    let mailOptions = {
+        from: '"nodemailer" <ait618group3@gmail.com>',
+        to: 'thanso2@students.towson.edu',
+        subject: 'You sent a message',
+        text: 'message'
+    };
+    res.status(200).send(transporter.sendMail(mailOptions))
+    });
