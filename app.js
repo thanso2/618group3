c01bfe0ccfad53b9e9adc8b2d20cf6dccc751c30
thanso2 <55329206+thanso2@users.noreply.github.com>
2019-11-25 19:56:49 -0500

    update

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const nodeMailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.listen(80, function(){
   console.log("Listening on port 80!")
@@ -14,20 +19,22 @@ app.get('/', function(req, res){
 
 
 app.post('/messages.html', function(req, res, next) {
+
+
     let transporter = nodeMailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
-            user: 'ait618group3@gmail.com',
-            pass: 'T0ws0n@iT618'
+            user: '',
+            pass: ''
         }
     })
     let mailOptions = {
-        from: '"nodemailer" <ait618group3@gmail.com>',
-        to: 'thanso2@students.towson.edu',
-        subject: 'You sent a message',
-        text: 'message'
+        from: '',
+        to: '',
+        subject: 'Message to Dr. ' + req.body.doctor,
+        text: req.body.message
     };
     res.status(200).send(transporter.sendMail(mailOptions))
     });
