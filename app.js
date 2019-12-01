138a30b5f194b8459ebe0ce2909fd719633c3a28
bhoffman823 <50034652+bhoffman823@users.noreply.github.com>
2019-12-01 14:23:13 -0500

    Update app.js
    Updated for DB inserts

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const nodeMailer = require('nodemailer');
 const bodyParser = require('body-parser');
+var mongodb = require('mongodb');
 
+var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
@@ -16,7 +18,32 @@ app.listen(80, function(){
 app.get('/', function(req, res){
     res.sendFile('index.html');
   });
+  
+app.post('/medicalrecords.html', function (req, res) {
+    dbConn.then(function(db) {
+        db.collection('medicalrecords').insertOne(req.body);
+		
+    });     
+});
+app.post('/updatecontactinfo.html', function (req, res) {
+  dbConn.then(function(db) {
+      db.collection('users').insertOne(req.body);
+  
+  });     
+});
 
+app.post('/createaccount.html', function (req, res) {
+    dbConn.then(function(db) {
+        db.collection('users').insertOne(req.body);
+		
+    }); 
+    return res.redirect("/");
+});
+app.post('/appointments.html', function (req, res) {
+  dbConn.then(function(db) {
+      db.collection('appointments').insertOne(req.body);
+    }); 
+  });
 
 app.post('/messages.html', function(req, res, next) {
 
